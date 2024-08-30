e565d2339542a16988cc85b36c363154cf3b01a0
vanessa1932 <l19290632@cdguzman.tecnm.mx>
2024-08-30 14:30:29 +0000

    crear archivo string

--- ejemplo.rb
+++ ejemplo.rb
@@ -1,7 +1,10 @@
 class Padre
+    def initialize (nombre)
+    @nombre = nombre 
+end 
     def  say_hello
-        saludo = "Hola desde #{self}"
-        return saludo
+       "Hola  #{@nombre} desde #{self}"
+      
     end
 end
 
@@ -9,8 +12,8 @@ class Hijo < Padre
 end
 
 
-saluda1 = Padre.new
+saluda1 = Padre.new ("Darth")
 puts saluda1.say_hello
 
-saludos2 = Hijo.new
+saludos2 = Hijo.new ("Anakin")
 puts saludos2.say_hello
---
+++ tarea.rb
@@ -0,0 +1,10 @@
+cadena = "Hola Mundo"
+
+longitud = cadena.length
+puts "La longitud de la cadena '#{cadena}' es: #{longitud}"
+
+cadena_mayusculas = cadena.upcase
+puts "La cadena en mayúsculas es: #{cadena_mayusculas}"
+
+cadena_invertida = cadena.reverse
+puts "La cadena invertida es: #{cadena_invertida}"
