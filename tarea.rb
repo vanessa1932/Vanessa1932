a4aca1f1bc06c995c715ea0c7cafc27183cb64d4
vanessa1932 <l19290632@cdguzman.tecnm.mx>
2024-08-30 14:37:39 +0000

    crear archivo string

--- tarea.rb
+++ tarea.rb
@@ -1,10 +1,11 @@
 cadena = "Hola Mundo"
 
-longitud = cadena.length
-puts "La longitud de la cadena '#{cadena}' es: #{longitud}"
+cadena = "Hola"
+puts cadena.replace("Mundo")  # => "adios"
+
 
 cadena_mayusculas = cadena.upcase
 puts "La cadena en mayúsculas es: #{cadena_mayusculas}"
 
-cadena_invertida = cadena.reverse
-puts "La cadena invertida es: #{cadena_invertida}"
+
+puts "Hola".concat(" Mundo")  # => "Hola Mundo"
